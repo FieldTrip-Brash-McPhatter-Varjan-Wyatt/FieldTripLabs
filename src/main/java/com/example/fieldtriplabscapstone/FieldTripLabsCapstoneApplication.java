fb0ff7f83dad75e7f73613a86432dba344e6a692
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-07-18 10:25:36 -0400

    initial start

---
+++ src/main/java/com/example/fieldtriplabscapstone/FieldTripLabsCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.fieldtriplabscapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldTripLabsCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldTripLabsCapstoneApplication.class, args);
+    }
+
+}
