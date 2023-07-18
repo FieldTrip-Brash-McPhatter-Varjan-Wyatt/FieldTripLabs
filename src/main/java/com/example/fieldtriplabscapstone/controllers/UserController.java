7bbbd2abb231eb9c1d1dd72389038657f62f00e4
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-07-18 16:12:32 -0400

    added more to models

---
+++ src/main/java/com/example/fieldtriplabscapstone/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.example.fieldtriplabscapstone.controllers;
+
+import com.example.fieldtriplabscapstone.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserController {
+    private UserRepository userDao;
+    private PasswordEncoder passwordEncoder;
+}
---
+++ src/main/java/com/example/fieldtriplabscapstone/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.fieldtriplabscapstone.repositories;
+
+
+
+
+import com.example.fieldtriplabscapstone.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
+
