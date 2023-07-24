372942ec3dd16919bd0b27f1e7686615595e1438
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-07-24 10:19:23 -0400

    fixed functionality

--- src/main/java/com/example/fieldtriplabscapstone/controllers/ItineraryController.java
+++ src/main/java/com/example/fieldtriplabscapstone/controllers/ItineraryController.java
@@ -1,8 +1,16 @@
 package com.example.fieldtriplabscapstone.controllers;
 
+import com.example.fieldtriplabscapstone.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItineraryController {
+    @GetMapping("/itinerary")
+    public String showItinerary(){
+        return "itinerary/index";
+    }
+
 
 }
--- src/main/java/com/example/fieldtriplabscapstone/models/ChecklistItems.java
+++ src/main/java/com/example/fieldtriplabscapstone/models/ChecklistItems.java
@@ -5,27 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.mapping.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="checklists")
-public class Checklist {
+@Table(name="checklist_items")
+public class ChecklistItems {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false, length = 100)
-    private String name;
+    private String itemName;
 
-    @Column
-    private boolean confirm;
-
-    @ManyToOne
-    @JoinColumn(name = "itinerary_id", nullable = false)
-    private Itinerary itinerary;
 }
