9878a7341def8def6c9e7d36cd3fdf6ca76752bc
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-07-25 10:09:55 -0400

    added some styling

--- src/main/java/com/example/fieldtriplabscapstone/controllers/ItineraryController.java
+++ src/main/java/com/example/fieldtriplabscapstone/controllers/ItineraryController.java
@@ -1,39 +1,40 @@
 package com.example.fieldtriplabscapstone.controllers;
 
 import com.example.fieldtriplabscapstone.models.Itinerary;
+import com.example.fieldtriplabscapstone.models.User;
 import com.example.fieldtriplabscapstone.repositories.ItineraryRepository;
 import com.example.fieldtriplabscapstone.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Optional;
+@AllArgsConstructor
 @Controller
 public class ItineraryController {
-    UserRepository userDao;
     ItineraryRepository itineraryDao;
+    UserRepository userDao;
+
     @GetMapping("/itinerary")
     public String showItinerary(){
         return "itinerary/index";
     }
 
     @GetMapping("/itinerary/{id}/edit")
-    public String editItinerary(@PathVariable Long id, @ModelAttribute Itinerary itinerary){
-
-//        Optional<Itinerary> optionalItinerary = itineraryDao.findById(id);
-//        if (optionalItinerary.isEmpty()){
-//            return "error";
-//        }
-//        Optional<User> optionalUser = userDao.findById(itinerary.getUser().getId());
-//        if (optionalUser.isEmpty()){
-//            return "error";
-//        }
-//        if (optionalUser.get().getId() != itinerary.getId()){
-//            return "error";
-//        }
-        return "itinerary/show";
+    public String showItineraryEdit(@ModelAttribute Itinerary itinerary, @PathVariable Long id){
+        Optional<Itinerary> optionalItinerary = itineraryDao.findById(id);
+        User loggedIn = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (optionalItinerary.isEmpty()){
+            return "error";
+        }
+        if (loggedIn.getId() != optionalItinerary.get().getUser().getId()){
+            return "error";
+        }
+        return "itinerary/edit";
     }
-
 
 
 }
