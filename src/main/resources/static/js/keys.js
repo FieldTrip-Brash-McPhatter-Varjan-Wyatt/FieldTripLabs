ccbe232a47f94ebe9cf95a27c44da15a818f5d30
Timothy Varjan <tim.varjan@gmail.com>
2023-08-03 12:43:35 -0300

    itinerary page design edits

--- src/main/resources/static/js/keys.js
+++ src/main/resources/static/js/keys.js
@@ -2,3 +2,4 @@ var config = {
     WEATHER_KEY : 'b4427c033bmshb38ab485978438ep198683jsn71d3fc98b8b1',
     FILESTACK_KEY : 'A87r7oKaT6i0PJ9UGEci7z'
 }
+// /etc/letsencrypt/live/fieldtriplab.com/fullchain.pem
